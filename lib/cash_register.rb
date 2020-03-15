c4124a7de8574610df3434144cf14da069645e51
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 20:51:03 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,63 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
 
+  def initialize(discount = nil)
+    self.total = 0
+    self.discount = discount
+    self.items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times { self.items << title }
+    self.last_transaction = [title, price, quantity]
+  end
+
+  def apply_discount
+    self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total = self.total * 8 / 10 }."
+  end
+
+  def void_last_transaction
+    self.total -= self.last_transaction[1] * self.last_transaction[2]
+    self.last_transaction[2].times do
+      self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
+    end
+  end
+
+end
+
+#So many ways to complete...
+
+# class CashRegister
+#   attr_accessor :total, :discount, :price, :items
+#   def initialize(discount = 0)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+#   def add_item(item, price, quantity = 1)
+#     @price = price
+#     @total += price * quantity
+#     if quantity > 1
+#       counter = 0
+#       while counter < quantity
+#         @items << item
+#         counter += 1
+#       end
+#     else
+#       @items << item
+#     end
+#   end
+#   def apply_discount
+#     if @discount > 0
+#       @discount_amount = (price * discount)/100
+#       @total -= @discount_amount
+#       return "After the discount, the total comes to $#{total}."
+#     else
+#       return "There is no discount to apply."
+#     end
+#   end
+#   def void_last_transaction
+#     @total -= @price
+#   end
+# end
